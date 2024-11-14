d36e17aeefdfff44a8cc446ab0f10f504a0a87a2
Steven Rieder <steven@rieder.nl>
2024-11-14 13:41:04 +0100

    update __init__.py

--- src/sunbather/__init__.py
+++ src/sunbather/__init__.py
@@ -2,39 +2,76 @@
 Initialize sunbather
 """
 import os
+import pathlib
+import shutil
 
-# Ensure Cloudy exists
-try:
-    CLOUDYVERSION = os.environ["CLOUDY_VERSION"]
-except KeyError:
-    CLOUDYVERSION = "23.01"
-SUNBATHERPATH = os.path.dirname(
-    os.path.abspath(__file__)
-)  # the absolute path where this code lives
-try:
-    # the path where Cloudy is installed
-    CLOUDYPATH = os.environ["CLOUDY_PATH"]
-except KeyError as exc:
-    CLOUDYPATH = f"{SUNBATHERPATH}/cloudy/c{CLOUDYVERSION}"
-if not os.path.exists(f"{CLOUDYPATH}/source/cloudy.exe"):
-    q = input(
-        f"Cloudy not found and CLOUDY_PATH is not set. "
-        f"Do you want to install Cloudy {CLOUDYVERSION} now in the Sunbather path? "
-        f"(y/n) "
-    )
+def install_cloudy():
+    """
+    Checks if Cloudy executable exists, and if not, prompts to download and build it.
+    """
+    try:
+        CLOUDYVERSION = os.environ["CLOUDY_VERSION"]
+    except KeyError:
+        CLOUDYVERSION = "23.01"
+    SUNBATHERPATH = os.path.dirname(
+        os.path.abspath(__file__)
+    )  # the absolute path where this code lives
+    try:
+        # the path where Cloudy is installed
+        CLOUDYPATH = os.environ["CLOUDY_PATH"]
+    except KeyError as exc:
+        CLOUDYPATH = f"{SUNBATHERPATH}/cloudy/c{CLOUDYVERSION}"
+    if not os.path.exists(f"{CLOUDYPATH}/source/cloudy.exe"):
+        q = input(
+            f"Cloudy not found and CLOUDY_PATH is not set. "
+            f"Do you want to install Cloudy {CLOUDYVERSION} now in the Sunbather path? "
+            f"(y/n) "
+        )
+        while q.lower() not in ["y", "n"]:
+            q = input("Please enter 'y' or 'n'")
+        if q == "n":
+            raise KeyError(
+                "Cloudy not found, and the environment variable 'CLOUDY_PATH' is not set. "
+                "Please set this variable in your .bashrc/.zshrc file "
+                "to the path where the Cloudy installation is located. "
+                "Do not point it to the /source/ subfolder, but to the main folder."
+            ) from exc
+        from sunbather.install_cloudy import GetCloudy
+        INSTALLER = GetCloudy(version=CLOUDYVERSION)
+        INSTALLER.download()
+        INSTALLER.extract()
+        INSTALLER.compile()
+        INSTALLER.test()
+        INSTALLER.copy_data()
+
+
+def make_workingdir():
+    """
+    Checks if the SUNBATHER_PROJECT_PATH environment variable has been set and
+    asks for input if not. Also asks to copy the default files to the working dir.
+    """
+    try:
+        workingdir = os.environ["SUNBATHER_PROJECT_PATH"]
+    except KeyError:
+        workingdir = input("Enter the working dir for Sunbather: ")
+    q = input(f"Copy default files to the working dir ({workingdir})? (y/n) ")
     while q.lower() not in ["y", "n"]:
-        q = input("Please enter 'y' or 'n'")
+        q = input("Please enter 'y' or 'n': ")
     if q == "n":
-        raise KeyError(
-            "Cloudy not found, and the environment variable 'CLOUDY_PATH' is not set. "
-            "Please set this variable in your .bashrc/.zshrc file "
-            "to the path where the Cloudy installation is located. "
-            "Do not point it to the /source/ subfolder, but to the main folder."
-        ) from exc
-    from sunbather.install_cloudy import GetCloudy
-    INSTALLER = GetCloudy(version=CLOUDYVERSION)
-    INSTALLER.download()
-    INSTALLER.extract()
-    INSTALLER.compile()
-    INSTALLER.test()
-    INSTALLER.copy_data()
+        return
+
+    sunbatherpath = f"{pathlib.Path(__file__).parent.resolve()}"
+    shutil.copytree(
+        sunbatherpath + "/data/workingdir",
+        workingdir,
+    )
+
+
+def firstrun():
+    """
+    Runs 'install_cloudy()' and 'make_workingdir()'.
+    """
+    install_cloudy()
+    make_workingdir()
+
+firstrun()
